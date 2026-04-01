3a580877349c6800896f1f1042fd001c90cafb8c
manami <savon.de.46@gmail.com>
2026-04-01 20:55:54 +0900

    style: 体調記録一覧デザイン修正

--- app/helpers/health_records_helper.rb
+++ app/helpers/health_records_helper.rb
@@ -1,17 +1,17 @@
 module HealthRecordsHelper
   def feeling_icon(value)
     case value
-    when 1 then "😊" # リラックス
-    when 2 then "😄" # 楽しい
-    when 3 then "😐" # ふつう
-    when 4 then "😥" # 落ち込み
+    when 1 then "😌" # おだやか
+    when 2 then "😆" # 楽しい
+    when 3 then "🙂" # ふつう
+    when 4 then "😞" # しんどい
     when 5 then "😡" # イライラ
     else ""
     end
   end
 
   def feeling_label(value)
-    labels = { 1 => "リラックス", 2 => "楽しい", 3 => "ふつう", 4 => "落ち込み", 5 => "イライラ" }
-    labels[value] || "不明"
+    labels = { 1 => "おだやか", 2 => "楽しい", 3 => "ふつう", 4 => "しんどい", 5 => "イライラ" }
+    labels[value] || "記録なし"
   end
 end
